7cdf7174ba14fa0c70006166fadada6c7633135c
Katye Russell <katye.russell@gmail.com>
2015-05-05 16:35:09 -0500

    Minor refactors to exit methods.

--- test/test_cheers_integration.rb
+++ test/test_cheers_integration.rb
@@ -22,18 +22,27 @@ Give me an... I
 Give me a... Z
 Give me an... A
 Eliza’s just GRAND!
+
 EOS
     assert_equal expected, output
   end
 
 
   def test_valid_name_invalid_birthday
-    output = `./cheers Eliza 19/05`
+    output = `./cheers Eliza 30/05`
     expected = <<EOS
-I would wish you a Happy Birthday if I knew when it was.
+Give me an... E
+Give me an... L
+Give me an... I
+Give me a... Z
+Give me an... A
+Eliza’s just GRAND!
+
+I would also wish you a Happy Birthday if I knew when it was.
 EOS
     assert_equal expected, output
   end
+
 
   def test_invalid_name_only
     output = `./cheers *#?@ `
@@ -65,7 +74,7 @@ Give me a... Z
 Give me an... A
 Eliza’s just GRAND!
 
-Awesome!  Your birthday is 125 days away!  Happy Birthday in advance!
+Awesome!  Your birthday is 112 days away!  Happy Birthday in advance!
 EOS
     assert_equal expected, output
   end
@@ -111,7 +120,7 @@ Give me an... N
 Give me an... N
 Mary-Ann’s just GRAND!
 
-Awesome!  Your birthday is 125 days away!  Happy Birthday in advance!
+Awesome!  Your birthday is 112 days away!  Happy Birthday in advance!
 EOS
     assert_equal expected, output
   end
@@ -126,19 +135,6 @@ EOS
     assert_equal expected, output
   end
 
-  def test_valid_name_invalid_birthday  # Failing (as it should be)
-    output = `./cheers Eliza 08/15/2015`
-    expected =  <<EOS
-Give me an... E
-Give me an... L
-Give me an... I
-Give me a... Z
-Give me an... A
-Eliza’s just GRAND!
 
-I would wish you a Happy Birthday if I knew when it was.
-EOS
-    assert_equal expected, output
-  end
 
 end
